1d750ee18f3d9c8235a6098fea12c53aee0d3325
caternuson <caternuson@gmail.com>
2021-09-08 15:18:42 -0700

    convert to busio

--- examples/am2315test/am2315test.ino
+++ examples/am2315test/am2315test.ino
@@ -1,19 +1,18 @@
-#include <Wire.h>
 #include <Adafruit_AM2315.h>
 
-/*************************************************** 
+/***************************************************
   This is an example for the AM2315 Humidity + Temp sensor
 
-  Designed specifically to work with the Adafruit AM2315 Sensor 
+  Designed specifically to work with the Adafruit AM2315 Sensor
   ----> https://www.adafruit.com/products/1293
 
-  These displays use I2C to communicate, 2 pins are required to  
+  These displays use I2C to communicate, 2 pins are required to
   interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  Written by Limor Fried/Ladyada for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
@@ -35,6 +34,9 @@ void setup() {
      Serial.println("Sensor not found, check wiring & pullups!");
      while (1);
   }
+
+  // begin() does a test read, so need to wait 2secs before first read
+  delay(2000);
 }
 
 void loop() {
